364e2ff2633a4fda4d0388f82c0c63cacaefecce
Denys Bilonozhko <deformix@gmail.com>
2025-04-23 20:27:21 +0300

    feat: implement file creation logic in create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { writeFile } from 'node:fs';
+
 const create = async () => {
-    // Write your code here 
+  const text = 'I am fresh and young';
+  const path = 'src/fs/files/';
+  const file = 'fresh.txt';
+
+  writeFile(`${path}${file}`, text, { flag: 'wx' }, (err) => {
+    if (err) {
+      if (err.code === 'EEXIST') {
+        throw new Error('FS operation failed');
+      } 
+        console.error(err);
+      
+    } else {
+      console.log(`${path}${file} created successfully`);
+    }
+  });
 };
 
 await create();
