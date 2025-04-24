d16ebd151b4de7450b7fe9b53ba1e4ce215e2b75
Denys Bilonozhko <deformix@gmail.com>
2025-04-24 11:34:13 +0300

    feat: implement file deletion logic in delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { rm, access, constants } from 'fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+  const file = 'src/fs/files/fileToRemove.txt';
+  const isFileExists = await access(file, constants.F_OK)
+    .then(() => true)
+    .catch(() => false);
+
+  if (!isFileExists) {
+    throw new Error('FS operation failed');
+  }
+
+  await rm(file).catch((err) => {
+    console.error(err);
+  });
 };
 
 await remove();
