82be8f4aae4613acfdf0f52cf53a9886ac50e703
Denys Bilonozhko <deformix@gmail.com>
2025-04-24 11:38:35 +0300

    feat: implement file listing functionality in list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+import { readdir } from 'fs/promises';
+
 const list = async () => {
-    // Write your code here 
+  const directory = 'src/fs/files';
+
+  try {
+    const files = await readdir(directory);
+    console.log(files);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
