3a8aef4cca9ab605396ce7a485822afbf63400fd
Denys Bilonozhko <deformix@gmail.com>
2025-04-25 14:42:15 +0300

    feat: enhance file reading functionality in read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { createReadStream } from 'fs';
+
 const read = async () => {
-    // Write your code here 
+  const source = new URL('./files/fileToRead.txt', import.meta.url);
+  const stream = createReadStream(source);
+
+  stream.on('data', (chunk) => {
+    process.stdout.write(chunk);
+  });
 };
 
 await read();
